87788d3400b15bae4128a9b6d873f01e3365a1a4
Yang Liu <liuyang12@focusmedia.cn>
2019-01-04 18:15:21 +0800

    update

--- train.py
+++ train.py
@@ -1,10 +1,8 @@
-import json
 import time
 
 import torch.optim
 import torch.utils.data
 from torch import nn
-from torch.nn.utils.rnn import pack_padded_sequence
 
 from data_gen import AGDataset
 from models import AGModel
@@ -12,16 +10,7 @@ from utils import *
 
 
 def main():
-    """
-    Training and validation.
-    """
-
-    global best_bleu4, epochs_since_improvement, checkpoint, start_epoch, fine_tune_encoder, word_map
-
-    # Read word map
-    word_map_file = os.path.join(data_folder, 'WORDMAP.json')
-    with open(word_map_file, 'r') as j:
-        word_map = json.load(j)
+    global best_bleu4, epochs_since_improvement, checkpoint, start_epoch
 
     # Initialize / load checkpoint
     if checkpoint is None:
@@ -97,34 +86,29 @@ def train(train_loader, model, criterion, optimizer, epoch):
         # Move to GPU, if available
         imgs = imgs.to(device)
         ages = ages.to(device)
-        genders = genders.to(device)
+        # genders = genders.to(device)
+        targets = ages
 
         # Forward prop.
-        features = model(imgs)
+        scores = model(imgs)
 
         # Calculate loss
         loss = criterion(scores, targets)
 
-        # Add doubly stochastic attention regularization
-        loss += alpha_c * ((1. - alphas.sum(dim=1)) ** 2).mean()
-
         # Back prop.
         optimizer.zero_grad()
-        if optimizer is not None:
-            optimizer.zero_grad()
         loss.backward()
 
         # Clip gradients
-        if optimizer is not None:
-            clip_gradient(optimizer, grad_clip)
+        clip_gradient(optimizer, grad_clip)
 
         # Update weights
         optimizer.step()
 
         # Keep track of metrics
         top5 = accuracy(scores, targets, 5)
-        losses.update(loss.item(), sum(decode_lengths))
-        top5accs.update(top5, sum(decode_lengths))
+        losses.update(loss.item())
+        top5accs.update(top5)
         batch_time.update(time.time() - start)
 
         start = time.time()
@@ -150,41 +134,24 @@ def validate(val_loader, model, criterion):
 
     start = time.time()
 
-    references = list()  # references (true captions) for calculating BLEU-4 score
-    hypotheses = list()  # hypotheses (predictions)
-
     # Batches
-    for i, (imgs, caps, caplens, allcaps) in enumerate(val_loader):
-
+    for i, (imgs, ages, genders) in enumerate(val_loader):
         # Move to device, if available
         imgs = imgs.to(device)
-        caps = caps.to(device)
-        caplens = caplens.to(device)
+        ages = ages.to(device)
+        # genders = genders.to(device)
+        targets = ages
 
         # Forward prop.
-        if model is not None:
-            imgs = model(imgs)
-        scores, caps_sorted, decode_lengths, alphas, sort_ind = decoder(imgs, caps, caplens)
-
-        # Since we decoded starting with <start>, the targets are all words after <start>, up to <end>
-        targets = caps_sorted[:, 1:]
-
-        # Remove timesteps that we didn't decode at, or are pads
-        # pack_padded_sequence is an easy trick to do this
-        scores_copy = scores.clone()
-        scores, _ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
-        targets, _ = pack_padded_sequence(targets, decode_lengths, batch_first=True)
+        scores = model(imgs)
 
         # Calculate loss
         loss = criterion(scores, targets)
 
-        # Add doubly stochastic attention regularization
-        loss += alpha_c * ((1. - alphas.sum(dim=1)) ** 2).mean()
-
         # Keep track of metrics
-        losses.update(loss.item(), sum(decode_lengths))
+        losses.update(loss.item())
         top5 = accuracy(scores, targets, 5)
-        top5accs.update(top5, sum(decode_lengths))
+        top5accs.update(top5)
         batch_time.update(time.time() - start)
 
         start = time.time()
@@ -196,37 +163,7 @@ def validate(val_loader, model, criterion):
                   'Top-5 Accuracy {top5.val:.3f} ({top5.avg:.3f})\t'.format(i, len(val_loader), batch_time=batch_time,
                                                                             loss=losses, top5=top5accs))
 
-        # Store references (true captions), and hypothesis (prediction) for each image
-        # If for n images, we have n hypotheses, and references a, b, c... for each image, we need -
-        # references = [[ref1a, ref1b, ref1c], [ref2a, ref2b], ...], hypotheses = [hyp1, hyp2, ...]
-
-        # References
-        allcaps = allcaps[sort_ind]  # because images were sorted in the decoder
-        for j in range(allcaps.shape[0]):
-            img_caps = allcaps[j].tolist()
-            img_captions = list(
-                map(lambda c: [w for w in c if w not in {word_map['<start>'], word_map['<pad>']}],
-                    img_caps))  # remove <start> and pads
-            references.append(img_captions)
-
-        # Hypotheses
-        _, preds = torch.max(scores_copy, dim=2)
-        preds = preds.tolist()
-        temp_preds = list()
-        for j, p in enumerate(preds):
-            temp_preds.append(preds[j][:decode_lengths[j]])  # remove pads
-        preds = temp_preds
-        hypotheses.extend(preds)
-
-        assert len(references) == len(hypotheses)
-
-    print(
-        '\n * LOSS - {loss.avg:.3f}, TOP-5 ACCURACY - {top5.avg:.3f}, BLEU-4 - {bleu}\n'.format(
-            loss=losses,
-            top5=top5accs,
-            bleu=bleu4))
-
-    return bleu4
+    return losses.avg
 
 
 if __name__ == '__main__':

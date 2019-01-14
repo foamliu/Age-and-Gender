674224d680d9d648724048f5ff8f33a4b82b6e52
Yang Liu <liuyang12@focusmedia.cn>
2019-01-14 16:00:56 +0800

    update

--- models.py
+++ models.py
@@ -15,23 +15,23 @@ class AgeGenPredModel(nn.Module):
         # Remove linear and pool layers (since we're not doing classification)
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
-        self.fc1 = nn.Linear(6144, 512)
-        self.age_pred = nn.Linear(512, age_num_classes)
+        self.fc1 = nn.Linear(2048, 2048)
+        self.age_pred = nn.Linear(2048, age_num_classes)
 
-        self.fc2 = nn.Linear(6144, 512)
-        self.gen_pred = nn.Linear(512, gen_num_classes)
+        self.fc2 = nn.Linear(2048, 2048)
+        self.gen_pred = nn.Linear(2048, gen_num_classes)
 
     def forward(self, images):
-        x = self.resnet(images)  # [N, 512, 4, 3]
-        # last_conv_out = x.view(-1, 6144)  # [N, 512]
-        #
-        # age_out = F.relu(self.fc1(last_conv_out))  # [N, 512]
-        # age_out = F.softmax(self.age_pred(age_out), dim=1)  # [N, 101]
-        #
-        # gen_out = F.relu(self.fc2(last_conv_out))  # [N, 512]
-        # gen_out = F.softmax(self.gen_pred(gen_out), dim=1)  # [N, 2]
+        x = self.resnet(images)  # [N, 2048, 1, 1]
+        last_conv_out = x.view(-1, 2048)  # [N, 2048]
 
-        return None, None # age_out, gen_out
+        age_out = F.relu(self.fc1(last_conv_out))  # [N, 2048]
+        age_out = F.softmax(self.age_pred(age_out), dim=1)  # [N, 101]
+
+        gen_out = F.relu(self.fc2(last_conv_out))  # [N, 2048]
+        gen_out = F.softmax(self.gen_pred(gen_out), dim=1)  # [N, 2]
+
+        return age_out, gen_out
 
 
 if __name__ == "__main__":

d3b25a066f666f556f173777481a24d9560c6b6d
Yang Liu <liuyang12@focusmedia.cn>
2019-01-14 11:07:05 +0800

    update

--- demo.py
+++ demo.py
@@ -1,8 +1,10 @@
 import pickle
 import random
+
 import numpy as np
-from config import *
+
 from align_faces import align_face
+from config import *
 
 if __name__ == "__main__":
     checkpoint = 'BEST_checkpoint_.pth.tar'
@@ -22,12 +24,21 @@ if __name__ == "__main__":
 
     samples = random.sample(samples, 10)
 
-    for sample in samples:
+    inputs = np.zeros((10, 3, 112, 96), dtype=np.float)
+
+    for i, sample in enumerate(samples):
         full_path = sample['full_path']
         landmarks = sample['landmarks']
+        real_age = sample['age']
+        real_gender = sample['gender']
         img = align_face(full_path, landmarks)
         img = img.transpose(2, 0, 1)
         assert img.shape == (3, image_h, image_w)
         assert np.max(img) <= 255
-        inputs = torch.FloatTensor(img / 255.)
-        age_out, gen_out = model(inputs)
+        inputs[i] = img / 255.
+
+    inputs = torch.FloatTensor(inputs)
+    inputs.to(device)
+    age_out, gen_out = model(inputs)
+    print('age_out: ' + str(age_out))
+    print('gen_out: ' + str(gen_out))
